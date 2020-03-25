786903891d9c1af5ae601aa2f25636c22bccf101
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2020-03-25 16:14:17 -0400

    updated feed stuff and integrated profile vc

--- RockHard/RockHard/Controllers/MainTabBarViewController.swift
+++ RockHard/RockHard/Controllers/MainTabBarViewController.swift
@@ -14,13 +14,14 @@ class MainTabBarController: UITabBarController {
     lazy var exerciseVC =  UINavigationController(rootViewController: ExerciseViewController())
     lazy var workoutVC =  UINavigationController(rootViewController: WorkoutViewController())
     lazy var buddyVC = UINavigationController(rootViewController: FindBuddyVC())
-    lazy var profileVC = UINavigationController(rootViewController: ProfileViewController())
+    lazy var profileVC = ProfileViewController()
 
     //MARK: -- ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBar.barStyle = .black
         self.tabBar.tintColor = #colorLiteral(red: 0.8403362632, green: 0.4548202157, blue: 0.4165673852, alpha: 1)
+        
         feedVC.tabBarItem = UITabBarItem(title: "Feed", image: nil, tag: 0)
         exerciseVC.tabBarItem = UITabBarItem(title: "Exercises", image: nil, tag: 1)
         workoutVC.tabBarItem = UITabBarItem(title: "Workouts", image: nil, tag: 2)
